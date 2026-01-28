2a5a857290d531f2d4323c91b3415250dff47add
raJayoux <128459480+JayGarland@users.noreply.github.com>
2026-01-28 10:37:37 +0100

    Remove environment.development.ts from tracking

--- apps/validator-portal/src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  validatorApiBaseUrl: 'https://localhost:5001',
-  supabaseUrl: 'https://dsljscddhcqlburhpwmz.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzbGpzY2RkaGNxbGJ1cmhwd216Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0NDg1MzAsImV4cCI6MjA4NTAyNDUzMH0.BiRRL4r3UFd2lCjXpU2xYY4opUeM5SryO9aSDfwqiEs'
-};
